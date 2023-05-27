796dc84446141ef7b9a3b6bf7077b16d0ee9c9f2
Raghav <raghapb34n@gmail.com>
2023-05-28 00:05:33 +0530

    worked on sidebar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
@@ -25,42 +27,152 @@ class BaseApp extends StatefulWidget {
 }
 
 class _BaseAppState extends State<BaseApp> {
+  double width = 75;
+  Color sidebarWidgetColorFirst = Color.fromARGB(255, 190, 145, 77);
+  Color sidebarWidgetColorSec = Color.fromARGB(255, 190, 145, 77);
+  Color sidebarIconColorFirst = Color.fromARGB(255, 72, 51, 43);
+  Color sidebarIconColorSec = Color.fromARGB(255, 72, 51, 43);
+
+  void _incWidth(PointerEnterEvent p) {
+    setState(() {
+      width = 150;
+    });
+  }
+
+  void _decWidth(PointerEnterEvent) {
+    setState(() {
+      width = 75;
+    });
+  }
+
+  List<Widget> _getChild(Icon icon, Text text) {
+    if (width == 150) {
+      return <Widget>[
+        icon,
+        AnimatedContainer(
+            width: width - 80,
+            duration: Duration(milliseconds: 200),
+            child: text)
+      ];
+    } else {
+      return <Widget>[icon];
+    }
+  }
+
+  void _changeColorFirst(PointerEnterEvent) {
+    if (sidebarWidgetColorFirst == Color.fromARGB(255, 190, 145, 77)) {
+      setState(() {
+        sidebarWidgetColorFirst = Color.fromARGB(255, 72, 51, 43);
+        sidebarIconColorFirst = Color.fromARGB(255, 190, 145, 77);
+      });
+    } else {
+      setState(() {
+        sidebarWidgetColorFirst = Color.fromARGB(255, 190, 145, 77);
+        sidebarIconColorFirst = Color.fromARGB(255, 72, 51, 43);
+      });
+    }
+  }
+
+  void _changeColorSecond(PointerEnterEvent) {
+    if (sidebarWidgetColorSec == Color.fromARGB(255, 190, 145, 77)) {
+      setState(() {
+        sidebarWidgetColorSec = Color.fromARGB(255, 72, 51, 43);
+        sidebarIconColorSec = Color.fromARGB(255, 190, 145, 77);
+      });
+    } else {
+      setState(() {
+        sidebarWidgetColorSec = Color.fromARGB(255, 190, 145, 77);
+        sidebarIconColorSec = Color.fromARGB(255, 72, 51, 43);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: NavDrawer(),
-        appBar: AppBar(
-          iconTheme: IconThemeData(color: Color.fromARGB(255, 210, 182, 149)),
-          title: Text('Lauda',
-              style: TextStyle(color: Color.fromARGB(255, 73, 79, 87))),
-          backgroundColor: Color.fromARGB(255, 251, 243, 226),
-        ),
-        backgroundColor: Color.fromARGB(255, 251, 243, 226),
-        body: Center(child: Text('Lauda')));
-  }
-}
-
-class NavDrawer extends StatefulWidget {
-  const NavDrawer({super.key});
-
-  @override
-  State<NavDrawer> createState() => _NavDrawerState();
-}
-
-class _NavDrawerState extends State<NavDrawer> {
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Color.fromARGB(255, 210, 182, 149),
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          ListTile(
-              leading: Icon(Icons.arrow_back),
-              title: Text('Back'),
-              onTap: () => {Navigator.of(context).pop()})
-        ],
-      ),
+      backgroundColor: Color.fromARGB(255, 251, 243, 226),
+      body: Row(children: [
+        Material(
+            elevation: 10,
+            child: MouseRegion(
+                onEnter: _incWidth,
+                onExit: _decWidth,
+                child: AnimatedContainer(
+                  color: Color.fromARGB(255, 210, 182, 149),
+                  width: width,
+                  duration: Duration(milliseconds: 200),
+                  child: ListView(
+                    children: <Widget>[
+                      MouseRegion(
+                          onEnter: _changeColorFirst,
+                          onExit: _changeColorFirst,
+                          child: Align(
+                              alignment: Alignment.topCenter,
+                              child: AnimatedContainer(
+                                  margin: EdgeInsets.all(10),
+                                  width: width - 25,
+                                  height: 50,
+                                  duration: Duration(milliseconds: 200),
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: sidebarWidgetColorFirst,
+                                  ),
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: _getChild(
+                                          Icon(
+                                            Icons.home,
+                                            color: sidebarIconColorFirst,
+                                          ),
+                                          Text(
+                                            "Home",
+                                            overflow: TextOverflow.fade,
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                color: sidebarIconColorFirst),
+                                          )))))),
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 200),
+                          color: Color.fromARGB(255, 190, 145, 77),
+                          height: 1,
+                          width: width - 10,
+                        ),
+                      ),
+                      MouseRegion(
+                          onEnter: _changeColorSecond,
+                          onExit: _changeColorSecond,
+                          child: Align(
+                              alignment: Alignment.topCenter,
+                              child: AnimatedContainer(
+                                  margin: EdgeInsets.all(10),
+                                  width: width - 25,
+                                  height: 50,
+                                  duration: Duration(milliseconds: 200),
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: sidebarWidgetColorSec,
+                                  ),
+                                  child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: _getChild(
+                                          Icon(Icons.lightbulb_outline,
+                                              color: sidebarIconColorSec),
+                                          Text("Generate Password",
+                                              overflow: TextOverflow.fade,
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                  color:
+                                                      sidebarIconColorSec)))))))
+                    ],
+                  ),
+                )))
+      ]),
     );
   }
 }
